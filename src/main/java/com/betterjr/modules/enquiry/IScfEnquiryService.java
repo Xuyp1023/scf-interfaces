de3311f524d217841b6e9585940fb8972b2ef947
Tangzw <tangzw@bytter.com>
2016-07-22 18:03:53 +0800

    询价，报价 新增，修改，查询

---
+++ src/main/java/com/betterjr/modules/enquiry/IScfEnquiryService.java
@@ -0,0 +1,52 @@
+package com.betterjr.modules.enquiry;
+
+import java.util.Map;
+
+public interface IScfEnquiryService {
+    
+    /**
+     * 查询询价列表
+     */
+    public String webQueryEnquiryList(Map<String, Object> anMap, int anFlag, int anPageNum, int anPageSize);
+
+    /**
+     * 新增询价
+     */
+    public String webAddEnquiry(Map<String, Object> anMap);
+
+    /**
+     * 查询询价详情
+     */
+    public String webFindEnquiryDetail(Long id);
+    
+    /**
+     * 修改询价
+     */
+    public String webSaveModifyEnquiry(Map<String, Object> anMap, Long anId);
+    
+    /**
+     * 查询报价列表
+     */
+    public String webQueryOfferList(Map<String, Object> anMap, int anFlag, int anPageNum, int anPageSize);
+
+    /**
+     * 新增报价
+     */
+    public String webAddOffer(Map<String, Object> anMap);
+    
+    /**
+     * 修改报价
+     */
+    public String webSaveModifyOffer(Map<String, Object> anMap, Long anId);
+
+    /**
+     * 查询报价详情
+     */
+    public String webFindOfferDetail(Long id);
+    
+    /**
+     * 获取已选择的订单
+     */
+    public String getSelectedOrders(Map<String, Object> anMap);
+
+}
