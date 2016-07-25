c66c5ec7e4278e4cc6c303629e3acbe4c28b18b5
Tangzw <tangzw@bytter.com>
2016-07-25 18:45:03 +0800

    询价，报价，参数设置，发货通知单

---
+++ src/main/java/com/betterjr/modules/repayment/IScfDeliveryNoticeService.java
@@ -0,0 +1,33 @@
+package com.betterjr.modules.repayment;
+
+import java.util.Map;
+
+public interface IScfDeliveryNoticeService {
+
+    /**
+     * 保存修改后的发货通知
+     * @param anMap
+     * @param anId
+     * @return
+     */
+    String webSaveModifyDeliveryNotice(Map<String, Object> anMap, Long anId);
+
+    /**
+     * 新增发货通知
+     * @param anMap
+     * @return
+     */
+    String webAddDeliveryNotice(Map<String, Object> anMap);
+
+    /**
+     * 查询发货通知列表
+     * @param anMap
+     * @param anFlag
+     * @param anPageNum
+     * @param anPageSize
+     * @return
+     */
+    String webQueryDeliveryNoticeList(Map<String, Object> anMap, int anFlag, int anPageNum, int anPageSize);
+  
+}
+
