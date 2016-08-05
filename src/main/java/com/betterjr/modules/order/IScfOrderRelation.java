17375789f86e1be3f387e91af2fd137f6bff9c43
wudy <wudy@bytter.com>
2016-08-05 15:38:39 +0800

    订单关系增加

---
+++ src/main/java/com/betterjr/modules/order/IScfOrderRelation.java
@@ -0,0 +1,11 @@
+package com.betterjr.modules.order;
+
+import java.util.Map;
+
+public interface IScfOrderRelation {
+    /**
+     * 订单关联关系增加
+     */
+    public String webAddOrderRelation(Map<String, Object> anMap);
+
+}
