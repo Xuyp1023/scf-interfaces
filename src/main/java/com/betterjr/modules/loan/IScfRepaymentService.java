4ae5ee8af0f767042cfebe5e6be1508913f7db01
Tangzw <tangzw@bytter.com>
2016-08-09 19:05:20 +0800

    融资

--- src/main/java/com/betterjr/modules/loan/IScfRepaymentService.java
+++ src/main/java/com/betterjr/modules/loan/IScfRepaymentService.java
@@ -3,7 +3,83 @@ package com.betterjr.modules.loan;
 import java.util.Map;
 
 public interface IScfRepaymentService {
+    /**
+     * 查询还款计划列表
+     * @param anMap
+     * @param anFlag
+     * @param anPageNum
+     * @param anPageSize
+     * @return
+     */
+    String webQueryPayPlanList(Map<String, Object> anMap, int anFlag, int anPageNum, int anPageSize);
 
+    /**
+     * 修改还款计划
+     * @param anMap
+     * @param id
+     * @return
+     */
+    String webSaveModifyPayPlan(Map<String, Object> anMap, Long id);
+
+    /**
+     * 查询还款计划详情
+     * @param anMap
+     * @param id
+     * @return
+     */
+    String webFindPayPlanDetail(Map<String, Object> anMap, Long id);
+
+    /**
+     * 还款记录-分页查询
+     * @param anMap
+     * @param anFlag
+     * @param anPageNum
+     * @param anPageSize
+     * @return
+     */
+    String webQueryPayRecordList(Map<String, Object> anMap, int anFlag, int anPageNum, int anPageSize);
+
+    /**
+     * 还款记录-修改
+     * @param anMap
+     * @param id
+     * @return
+     */
+    String webSaveModifyPayRecord(Map<String, Object> anMap, Long id);
+
+    /**
+     * 还款记录-详情
+     * @param anMap
+     * @param id
+     * @return
+     */
+    String webFindPayRecordDetail(Map<String, Object> anMap, Long id);
+
+    /**
+     * 还款分配记录-分页查询
+     * @param anMap
+     * @param anFlag
+     * @param anPageNum
+     * @param anPageSize
+     * @return
+     */
+    String webQueryRecordDetailList(Map<String, Object> anMap, int anFlag, int anPageNum, int anPageSize);
+
+    /**
+     * 还款分配记录-修改
+     * @param anMap
+     * @param id
+     * @return
+     */
+    String webSaveModifyRecordDetail(Map<String, Object> anMap, Long id);
+
+    /**
+     * 还款分配记录-详情
+     * @param anMap
+     * @param id
+     * @return
+     */
+    String webFindRecordDetail(Map<String, Object> anMap, Long id);
     
     /**
      * 保存还款
@@ -35,7 +111,6 @@ public interface IScfRepaymentService {
      * @return
      */
     String webCalculatPayType(String anRequestNo, String anPayDate);
-
 
 
 }
