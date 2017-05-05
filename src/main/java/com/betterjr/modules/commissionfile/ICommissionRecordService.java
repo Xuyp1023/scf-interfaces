8d96d67d250d43ef751ae9d48c9de52a3d8b4bb8
Xuyp <xuyp@bytter.com>
2017-05-05 18:40:55 +0800

    佣金记录

--- src/main/java/com/betterjr/modules/commissionfile/ICommissionRecordService.java
+++ src/main/java/com/betterjr/modules/commissionfile/ICommissionRecordService.java
@@ -1,5 +1,11 @@
 package com.betterjr.modules.commissionfile;
 
+import java.util.Map;
+
 public interface ICommissionRecordService {
+
+    String webQueryRecordList(Map<String, Object> anAnMap, String anFlag, int anPageNum, int anPageSize);
+
+    String webSaveAuditRecordList(Long anCustNo, String anImportDate);
 
 }
