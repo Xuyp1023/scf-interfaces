a7e5d6ec6aeca763dd4474358707ff0ffc58bd1c
Xuyp <xuyp@bytter.com>
2017-05-06 21:26:53 +0800

    投递账单

--- src/main/java/com/betterjr/modules/delivery/IDeliveryService.java
+++ src/main/java/com/betterjr/modules/delivery/IDeliveryService.java
@@ -1,5 +1,19 @@
 package com.betterjr.modules.delivery;
 
+import java.util.Map;
+
 public interface IDeliveryService {
+
+    String webQueryFileList(Map<String, Object> anAnMap, String anFlag, int anPageNum, int anPageSize, boolean anIsPostCust);
+
+    String webFindDeliveryRecord(String anRefNo);
+
+    String webAddDeliveryRecord(Map anParamMap);
+
+    String webSaveDeleteStatement(Long anStatementId);
+
+    String webSaveExpressDelivery(String anRefNo);
+
+    String webSaveConfirmDelivery(String anRefNo);
 
 }
