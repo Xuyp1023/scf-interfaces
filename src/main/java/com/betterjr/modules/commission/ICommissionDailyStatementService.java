c199b9f977e5c53eb73b12d31459110a3cf9c613
Hubl <hubl@bytter.com>
2017-05-09 09:42:49 +0800

    task-202-3

--- src/main/java/com/betterjr/modules/commission/ICommissionDailyStatementService.java
+++ src/main/java/com/betterjr/modules/commission/ICommissionDailyStatementService.java
@@ -49,4 +49,56 @@ public interface ICommissionDailyStatementService {
      */
     public String webDelDailyStatement(Long anDailyStatementId);
     
+    /***
+     * 查询佣金支付统计数据
+     * @param anPayDate
+     * @param anOwnCustNo
+     * @return
+     */
+    public String webFindPayResultCount(String anPayDate,Long anOwnCustNo);
+    
+    /***
+     * 分页查询支付记录
+     * @param anOwnCustNo
+     * @param anPayDate
+     * @param anFlag
+     * @param anPageNum
+     * @param anPageSize
+     * @return
+     */
+    public String webQueryPayResultRecord(Long anOwnCustNo,String anPayDate,int anFlag, int anPageNum, int anPageSize);
+    
+    /***
+     * 根据支付日期查询佣金支付记录
+     * @param anPayDate
+     * @param anOwnCustNo
+     * @return
+     */
+    public String webFindPayResultInfo(String anPayDate,Long anOwnCustNo);
+    
+    /***
+     * 添加日账单信息
+     * @param anDailyRefNo
+     * @param anPayDate
+     * @param anOwnCustNo
+     * @return
+     */
+    public String webSaveDailyStatement(String anDailyRefNo,String anPayDate,Long anOwnCustNo);
+    
+    /***
+     * 查询日账单详情
+     * @param anDailyStatementId
+     * @return
+     */
+    public String webFindDailyStatementById(Long anDailyStatementId);
+
+    /***
+     * 根据ID查询日账单记录
+     * @param anDailyStatementId
+     * @param anPageNum
+     * @param anPageSize
+     * @param anFlag
+     * @return
+     */
+    public String webQueryDailyStatementRecordById(Long anDailyStatementId,int anPageNum,int anPageSize,String anFlag);
 }
