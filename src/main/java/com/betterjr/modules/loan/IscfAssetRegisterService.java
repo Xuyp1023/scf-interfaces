acd376e997b5d60ce4bde68c7c700feb7b5f8395
Tangzw <tangzw@bytter.com>
2017-05-16 17:58:05 +0800

    融资申请临时表

---
+++ src/main/java/com/betterjr/modules/loan/IscfAssetRegisterService.java
@@ -0,0 +1,39 @@
+package com.betterjr.modules.loan;
+
+import java.util.Map;
+
+/**
+ * 中登网，资产登记与资产检查相关接口
+ * @author hewei
+ *
+ */
+public interface IscfAssetRegisterService {
+	
+    /**
+     * 新增资产登记
+     * @param anMap
+     * @return
+     */
+    String webAddAssetRegister(Map<String, Object> anMap);
+
+    /**
+     * 查询资产登记
+     * @param anRequestNo
+     * @return
+     */
+    String webFindAssetRegister(String anRequestNo);
+    
+    /**
+     * 新增资产检查
+     * @param anMap
+     * @return
+     */
+    String webAddAssetCheck(Map<String, Object> anMap);
+
+    /**
+     * 查询资产检查
+     * @param anRequestNo
+     * @return
+     */
+    String webFindAssetCheck(String anRequestNo);
+}
