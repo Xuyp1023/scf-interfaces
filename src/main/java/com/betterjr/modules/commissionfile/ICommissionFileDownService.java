f3639162641fa758bc08cf540761aad056618eac
Xuyp <xuyp@bytter.com>
2017-06-02 14:16:30 +0800

    test

--- src/main/java/com/betterjr/modules/commissionfile/ICommissionFileDownService.java
+++ src/main/java/com/betterjr/modules/commissionfile/ICommissionFileDownService.java
@@ -5,5 +5,11 @@ import java.util.Map;
 public interface ICommissionFileDownService {
     
     String webQueryFileDownList(Map<String, Object> anAnMap, String anFlag, int anPageNum, int anPageSize);
+   
+    String webQueryCanAuditFileList(Map<String, Object> anAnMap, String anFlag, int anPageNum, int anPageSize);
+
+    String webQueryFileRecordByFileId(Long anFileId, String anFlag, int anPageNum, int anPageSize);
+
+    String webSaveAuditFileDownById(Map<String, Object> anAnMap);
 
 }
