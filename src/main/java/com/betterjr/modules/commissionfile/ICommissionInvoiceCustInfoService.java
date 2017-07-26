2a49ef2338cfe7db98b8947df3bb9471d552c425
Xuyp <xuyp@bytter.com>
2017-07-26 09:47:35 +0800

    task216  发票

---
+++ src/main/java/com/betterjr/modules/commissionfile/ICommissionInvoiceCustInfoService.java
@@ -0,0 +1,17 @@
+package com.betterjr.modules.commissionfile;
+
+import java.util.Map;
+
+public interface ICommissionInvoiceCustInfoService {
+
+    String webSaveAddInvoiceCustInfo(Map anParamMap);
+
+    String webQueryInvoiceCustInfoList(Map<String, Object> anAnMap, String anFlag, int anPageNum, int anPageSize);
+
+    String webSaveUpdateInvoiceCustInfo(Map anParamMap);
+    
+    String webSaveDeleteCustInfo(Long anCustInfoId);
+    
+    String webFindInvoiceCustInfoEffectiveByCustNo(Long anCustNo,Long anCoreCustNo);
+    
+}
